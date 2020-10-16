41c0976e8aa8863b05cdf20e592f4edfeab00972
maulana01 <maulanaimammalik4@gmail.com>
2020-10-16 14:56:07 +0700

    new commit

---
+++ src/if1/pkg10119010/latihan14/bit/IF110119010Latihan14Bit.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119010.latihan14.bit;
+
+/**
+ *
+ * @author
+ * NAMA     : Maulana Imam Malik
+ * KELAS    : IF1
+ * NIM      : 10119010
+ * Deskripsi Program : Program ini berisi program untuk menampilkan cara kerja
+ *                     operator bit di dalam java
+ */
+public class IF110119010Latihan14Bit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 60;/* 60 = 0011 1100*/
+        int b = 13;/* 13 = 0000 1101*/
+        int c = 0;
+        c = a & b;
+        /* 12 = 0000 1100 */
+        System.out.println("a & b = " + c);
+        c = a | b;
+        /* 61 = 0011 1101 */
+        System.out.println("a | b = " + c);
+        c = a ^ b;
+        /* 49 = 0011 0001 */
+        System.out.println("a ^ b = " + c);
+        c = ~a;
+        /*-61 = 1100 0011 */
+        System.out.println("~a = " + c);
+        c = a << 2;
+        /* 240 = 1100 0000 */
+        System.out.println("a << 2 = " + c);
+        c = a >> 2;
+        /* 15 = 0000 1111 */
+        System.out.println("a >> 2 = " + c);
+    }
+    
+}
